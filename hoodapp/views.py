6a6fd0cfaedf9044050c37e9e5314e93ad84bba6
Robin <robinkaris7@gmail.com>
2019-10-29 22:59:22 +0300

    this commit makes changes to views

--- hoodapp/views.py
+++ hoodapp/views.py
@@ -1,28 +1,27 @@
 from django.shortcuts import render,redirect
+from django.contrib.auth.decorators import login_required
 from django.core.exceptions import ObjectDoesNotExist
+from django.http import Http404
 from .models import *
 from .forms import *
 # Create your views here.
+
+
 def home(request):
 
-         
+    current_user = request.user    
+    profile = Profile.objects.filter(prof_user=request.user)
+    hoodie = Neighborhood.objects.all()
+    arr=[]
+    for post in profile:
+        arr.append(post.hood_id.id)
+    if len(arr)>0:
+        id=arr[0]
+        all_posts = Post.objects.filter(hood_post=id)
+    else:
+        all_posts = ""       
 
-    return render(request,'index.html')
-
-
-
-
-
-def profile(request):
-    current_user = request.user
-    business = Business.objects.filter(business_owner = current_user)
-    posts = Post.objects.filter(post_owner = current_user)
-    try:   
-        prof = Profile.objects.get(prof_user=current_user)
-    except ObjectDoesNotExist:
-        return redirect('new_profile')
-
-    return render(request,'profile.html',{'profile':prof,'business':business,'posts':posts})
+    return render(request,'index.html',{"all_posts":all_posts,"prof_info":profile,"hoodie":hoodie})
 
 
 def new_profile(request):
@@ -37,7 +36,7 @@ def new_profile(request):
         return redirect('profile')
     else:
         form = ProfileForm()
-    return render(request, 'new_profile.html', {"form": form})    
+    return render(request, 'new_profile.html', {"form": form})
 
 
 def profile_edit(request):
@@ -51,3 +50,77 @@ def profile_edit(request):
     else:
         form = ProfileForm()
     return render(request,'edit_profile.html',{'form':form})
+
+
+def profile(request):
+    current_user = request.user
+    business = Business.objects.filter(business_owner = current_user)
+    posts = Post.objects.filter(post_owner = current_user)
+    try:   
+        prof = Profile.objects.get(prof_user=current_user)
+    except ObjectDoesNotExist:
+        return redirect('new_profile')
+
+    return render(request,'profile.html',{'profile':prof,'business':business,'posts':posts})
+
+def new_business(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = BusinessForm(request.POST,request.FILES)
+        if form.is_valid():
+            business = form.save(commit=False)
+            business.business_owner = current_user
+            business.save()
+        return redirect('home')
+    else:
+        form = BusinessForm()
+    return render(request,"business/business_form.html",{"form":form})
+
+def business(request):
+    current_user = request.user
+    try:
+        profile = Profile.objects.filter(prof_user=request.user)
+        arr=[]
+        for business in profile:
+            arr.append(business.hood_id.id)
+        if len(arr)>0:
+            id=arr[0]
+            all_businesses = Business.objects.filter(business_hood_id=id)
+        else:
+            all_businesses = Business.objects.filter(business_hood_id=10000000000)
+    except Exception as e:
+        raise Http404()
+     
+    return render(request,'business/business_index.html',{"all_businesses":all_businesses,"profile":profile})
+
+def search_post(request):
+    if 'post' in request.GET and request.GET ["post"]:
+        search_term = request.GET.get("post")
+        searched_posts = Business.search_business_by_title(search_term)
+        message = f'{search_term}'
+
+        return render(request, 'search/search.html', {"message":message, "post":searched_posts})
+
+    else:
+        message = "No search results yet!"
+        return render (request, 'search/search.html', {"message": message})
+
+
+def contact(request):
+    contacts = ContactInfo.objects.all()
+    return render(request,'hood_app/contact_info.html',{"contacts":contacts})
+
+
+def new_post(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = PostForm(request.POST,request.FILES)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.post_owner = current_user
+            post.save()
+        return redirect('home')
+    else:
+        form = PostForm()
+    return render(request,"posts/create_post.html",{"form":form})
+
