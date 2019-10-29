922daeb714c2f2a39e452092595f761a8bf872e8
Robin <robinkaris7@gmail.com>
2019-10-29 20:18:50 +0300

    this commit adds profile templates

--- hoodapp/views.py
+++ hoodapp/views.py
@@ -1,8 +1,23 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.core.exceptions import ObjectDoesNotExist
 # Create your views here.
 def home(request):
 
          
 
     return render(request,'index.html')
+
+
+
+
+
+def profile(request):
+    current_user = request.user
+    business = Business.objects.filter(business_owner = current_user)
+    posts = Post.objects.filter(post_owner = current_user)
+    try:   
+        prof = Profile.objects.get(prof_user=current_user)
+    except ObjectDoesNotExist:
+        return redirect('new_profile')
+
+    return render(request,'profile.html',{'profile':prof,'business':business,'posts':posts})
