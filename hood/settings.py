86f2b5a74d75cc92374873b4a362b9e4a104a673
Robin <robinkaris7@gmail.com>
2019-10-29 23:48:09 +0300

    this commit adds static folder

--- hood/settings.py
+++ hood/settings.py
@@ -122,6 +122,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
+STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
-]
+)
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
